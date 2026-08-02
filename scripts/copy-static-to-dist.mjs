853d82aa1c35d937459de909ff892969a9dee458
Brody Glazer <brodyglazer@MacBook-Pro.local>
2026-08-02 00:09:56 -0700

    Add favicon set to all HTML pages, copy favicon files to dist via build script

--- scripts/copy-static-to-dist.mjs
+++ scripts/copy-static-to-dist.mjs
@@ -17,6 +17,13 @@ const directoriesToCopy = [
 
 const filesToCopy = [
   'donation.json',
+  'favicon.ico',
+  'favicon.svg',
+  'favicon-96x96.png',
+  'apple-touch-icon.png',
+  'web-app-manifest-192x192.png',
+  'web-app-manifest-512x512.png',
+  'site.webmanifest',
 ];
 
 async function exists(targetPath) {
